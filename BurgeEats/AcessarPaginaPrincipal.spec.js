82da08463a03d34ec303cc9f16f15a44dc15d26b
cedric arnaud <79386450+CedricArnaud20@users.noreply.github.com>
2024-06-10 23:20:43 -0300

    Ajuste primeiros testes adicionando xpath para mais acertividade no click

--- BurgeEats/AcessarPaginaPrincipal.spec.js
+++ BurgeEats/AcessarPaginaPrincipal.spec.js
@@ -12,7 +12,25 @@ test('Deve acessar a página de cadastro', async ({ page }) => {
   await expect(Titulo).toHaveText('Seja um parceiro entregador pela Buger Eats')
 
   //await page.getByRole('button').click();
+  await page.locator('//strong[text()="Cadastre-se para fazer entregas"]').click()
+})
+
+
+test('Deve voltar para home page', async ({page}) => {
+
+  await page.goto('/');
+
+  const Titulo = page.locator('h1')
+  
+  ////h1[text()="Seja um parceiro entregador pela Buger Eats"]
+
+  await expect(Titulo).toHaveText('Seja um parceiro entregador pela Buger Eats')
+
+  //await page.getByRole('button').click();
   await page.getByRole('link', { name: 'Cadastre-se para fazer' }).click()
+
+  await page.locator('//header/a[text()="Voltar para home"]').click()  
+  
 });
 
 
